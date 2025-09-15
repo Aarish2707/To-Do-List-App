abd19597600f9b736a8ae56a498694d0d39fce87
Aarish A. Sheikh <aarishsheikh3757@gmail.com>
2025-09-15 22:54:22 +0530

    Fix build configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './public/index.html'
+      }
+    }
   }
 })
